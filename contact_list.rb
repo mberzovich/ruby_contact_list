9d966680a7816b8f1f84e879b70a18e1abaa5dc6
Matt Berz <mberzovich@gmail.com>
2017-11-10 12:03:26 -0700

    user choice and menu working

--- contact_list.rb
+++ contact_list.rb
@@ -2,7 +2,21 @@ require 'pry'
 require 'colorize'
 
 puts 'Welcome o the contact list.'
+
 puts 'Menu'
 puts '1) Show Contacts'
 puts '2) Add Contacts'
 puts '3) Quit'
+
+user_input = gets.to_i
+case user_input
+  when 1
+    puts 'show all contact'
+  when 2
+    puts 'add a new contact'
+  when 3
+    puts 'thanks for contact listing...'
+    exit
+  else
+    puts 'Bad Input'
+end
